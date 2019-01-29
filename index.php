7726c979b66ded72a3162445dc8f5fc361c417cf
Pascal Chevrel <pascal.chevrel@free.fr>
2019-01-29 10:28:46 +0100

    move to bootstrap for styling, better defaults for tables

--- index.php
+++ index.php
@@ -1,13 +1,15 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
 	<title>Regressers</title>
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="stylesheet" href="https://unpkg.com/purecss@1.0.0/build/pure-min.css" integrity="sha384-nn4HPE8lTHyVtfCBi5yW9d20FjT8BJwUXyWZT9InLYax14RDjBj46LmSztkmNP9w" crossorigin="anonymous">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
 <style>
 </style>
 </head>
 <body>
+<div class="container-fluid">
 
 
 <?php
@@ -101,8 +103,8 @@ foreach($output as $key => $value) {
 	];
 }
 
-print "<h3>Bugs that causes regressions in " . secureText($version) . "</h3>
-<table class='pure-table pure-table-bordered'>
+print "<h3>Bugs that caused regressions in " . secureText($version) . "</h3>
+<table class='table table-striped table-sm'>
 <thead>
 <tr><th>#</th><th>Bug</th></tr>\n
 </thead>\n
@@ -117,5 +119,7 @@ foreach($final as $values) {
 print "</tbody>\n</table>";
 
 ?>
+
+</div>
 </body>
 </html>
